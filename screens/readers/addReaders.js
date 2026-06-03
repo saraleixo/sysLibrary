8009b22c14831c21431403c7251738a7cbaaa5e2
sara <aleixodasilvasara@gmail.com>
2026-06-02 23:15:08 -0300

    feat: cadastra novos leitores

---
+++ screens/readers/addReaders.js
@@ -0,0 +1,34 @@
+import { db } from "../../services/firebase.js";
+import {
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const form = document.getElementById("readerForm");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    try {
+        const name = document.getElementById("name").value;
+        const lastName = document.getElementById("lastName").value;
+        const phoneNumber = document.getElementById("phoneNumber").value;
+        const email = document.getElementById("email").value;
+        const date = document.getElementById("date").value;
+
+        await addDoc(collection(db, "readers"), {
+            name,
+            lastName,
+            phoneNumber,
+            email,
+            date
+        });
+
+        alert("Leitor cadastrado com sucesso!");
+        form.reset();
+
+    } catch (error) {
+        console.error(error);
+        alert("Erro ao cadastrar leitor.");
+    }
+});
